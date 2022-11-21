02379d68c5fdb74404738133ae0be8d0f5571213
Isaac Carnes <isaacjcarnes@gmail.com>
2022-11-21 17:05:53 -0500

    Basic setup for input without editable suffixes for clear usage

--- src/components/PreferencesForm/index.js
+++ src/components/PreferencesForm/index.js
@@ -1,54 +1,130 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
-const PreferencesForm = ({compClass, compProp, compValue, compIndex, SetProp}) => {
+const PreferencesForm = ({
+  compClass,
+  compProp,
+  compValue,
+  compIndex,
+  SetProp,
+}) => {
   const [currentVal, setCurrentVal] = useState(compValue);
+  const [valSuffix, setValSuffix] = useState("");
+
+  
   let formClass = "form" + compIndex;
-  let inp = <input 
-  type="text"
-  id={formClass}
-  placeholder="none"
-  defaultValue={currentVal}
-  onChange={(e) => {
-    e.preventDefault();
-    changeValue(compClass, compProp, e.target.value);
-  }}/>
+  const goodInp = () => {
+    if (
+      compProp !== "textContent" &&
+      compProp !== "backgroundColor" &&
+      compProp !== "color"
+    ) {
+      return (
+        <div className="InputWrapper">
+        <input
+          type="number"
+          id={formClass}
+          placeholder="none"
+          defaultValue={currentVal}
+          onChange={(e) => {
+            e.preventDefault();
+            console.log(e.target.value);
+            changeValue(compClass, compProp, e.target.value);
+          }}
+        />
+        <p className="suffix">{valSuffix}</p>
+        </div>
+      );
+    } else {
+      return(
+        <div className="InputWrapper">
+          <input
+        type="text"
+        id={formClass}
+        placeholder="none"
+        defaultValue={currentVal}
+        onChange={(e) => {
+          e.preventDefault();
+          console.log(e.target.value);
+          changeValue(compClass, compProp, e.target.value);
+        }}
+      />
+    </div>
+      )
+    }
+  };
+  let inp = (
+    <input
+      type="text"
+      id={formClass}
+      placeholder="none"
+      defaultValue={currentVal}
+      onChange={(e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+        changeValue(compClass, compProp, e.target.value);
+      }}
+    />
+  );
 
   const changeValue = (className, property, value) => {
-    SetProp(property, value);
+    let withSuffix = value + valSuffix;
+    SetProp(property, withSuffix);
     //React dom find by class name, replace property value with new value, or add value if not already in it
     let target = document.getElementsByClassName(className)[0];
-    if(target && compProp === "textContent"){
+    if (target && compProp === "textContent") {
       target.textContent = value;
       return true;
-    } if(target && compProp === "backgroundColor"){
+    }
+    if (target && compProp === "backgroundColor") {
       target.style.backgroundColor = value;
       return true;
-    } else if(target && compProp === "fontSize"){
-      target.style.fontSize = value;
+    } else if (target && compProp === "fontSize") {
+      target.style.fontSize = withSuffix;
       return true;
     } else if (target) {
-      target.style.setProperty(property, value);
+      target.style.setProperty(property, withSuffix);
       return true; //Success
     }
     return false; //False
   };
 
-  useEffect((compClass, compProp, currentVal) => {
-    changeValue(compClass, compProp, currentVal)
-  }, [currentVal])
-
-  if(currentVal !== compValue){
+  /*if (currentVal !== compValue) {
     //console.log(currentVal + " " + compValue)
-    document.getElementById(formClass).value = compValue;
-    setCurrentVal(compValue);
-  }
+    console.log("change value");
+    let suffixLength = 0;
+    if(compValue.substring(compValue.length-1) === "%"){
+      suffixLength = 1;
+    } else if (compValue.substring(compValue.length-2) === "px" || compValue.substring(compValue.length-2) === "in"){
+      suffixLength = 2;
+    }
+    document.getElementById(formClass).value = compValue.substring(0, compValue.length-suffixLength);
+    setCurrentVal(compValue.substring(0, compValue.length-suffixLength));
+  }*/
 
-  return(
-    <form onSubmit={e => e.preventDefault()}>
-      <label className='bg-secondary'>{compProp[0].toUpperCase() + compProp.substring(1).replace(/([a-z])([A-Z])/g, '$1 $2')}</label>
-      {inp}
+  useEffect(() => {
+    let suffixLength = 0;
+    if(compValue.substring(compValue.length-1) === "%"){
+      suffixLength = 1;
+      setValSuffix(compValue.substring(compValue.length-1));
+    } else if (compValue.substring(compValue.length-2) === "px" || compValue.substring(compValue.length-2) === "in"){
+      suffixLength = 2;
+      setValSuffix(compValue.substring(compValue.length-2));
+    } else {
+      setValSuffix("");
+    }
+    document.getElementById(formClass).value = compValue.substring(0, compValue.length-suffixLength);
+    setCurrentVal(compValue.substring(0, compValue.length-suffixLength));
+  }, [compClass]);
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label className="bg-secondary">
+        {compProp[0].toUpperCase() +
+          compProp.substring(1).replace(/([a-z])([A-Z])/g, "$1 $2")}
+      </label>
+      {goodInp()}
     </form>
-  )
+  );
 };
 
 export default PreferencesForm;
